6161f33aa581a4bbd7b7be4442671e1a36553d0a
alcacer <j.alcacer.2018@alumnos.urjc.es>
2021-04-09 12:54:39 +0200

    Ejemplo 7

--- P2/S5/Ej-07.js
+++ P2/S5/Ej-07.js
@@ -1,32 +1,32 @@
-//Elementos de la gui
+//-- Elementos de la gui
 const gui = {
     display : document.getElementById("display"),
     start : document.getElementById("start"),
     stop : document.getElementById("stop"),
-    reset: document.getElementById("reset")
+    reset : document.getElementById("reset")
 }
 
-console.log("Ejecutando JS...");
+console.log("Ejecuitando JS...");
 
-//Definir un objeto cronometro
-
+//-- Definir un objeto cronómetro
 const crono = new Crono(gui.display);
 
-//Configurar las funciones de retrollamada
-//Arranque del cronometro 
+//---- Configurar las funciones de retrollamada
+
+//-- Arranque del cronometro
 gui.start.onclick = () => {
     console.log("Start!!");
     crono.start();
 }
-
-//Detener el cronometro 
-gui.stop.onclick = () =>{
-    console.log("Stop!!");
+  
+//-- Detener el cronómetro
+gui.stop.onclick = () => {
+    console.log("Stop!");
     crono.stop();
 }
 
-//Reset del cronometro 
-gui.rest.onclick = () => {
-    console.log("Reset!!");
+//-- Reset del cronómetro
+gui.reset.onclick = () => {
+    console.log("Reset!");
     crono.reset();
 }
--- P2/S5/crono.js
+++ P2/S5/crono.js
@@ -1,61 +1,64 @@
-//Clase cronomero
-class Crono{
-    // Constructor. hay que indicar el 
-    //display donde mostrar el cronometro
+//-- Clase cronómetro
+class Crono {
+
+    //-- Constructor. Hay que indicar el 
+    //-- display donde mostrar el cronómetro
     constructor(display) {
         this.display = display;
-        
-        //Tiempo
-        this.cent = 0, //Centesimas
-        this.seg = 0, //Segundos
-        this.min = 0, //Minutos
-        this.time = 0; //Temporizador asociado
+
+        //-- Tiempo
+        this.cent = 0, //-- Centésimas
+        this.seg = 0,  //-- Segundos
+        this.min = 0,  //-- Minutos
+        this.timer = 0;  //-- Temporizador asociado
     }
 
-// Metodo que se ejecuta en una centesima
-Tic() {
-    //Incrementar en una centesima
-    this.cent +=1;
+    //-- Metodo que se ejecuta cada centésima
+    tic() {
+        //-- Incrementar en una centesima
+        this.cent += 1;
 
-    //100 centesimas hacen 1 segundo
-    if (this.cent == 100) {
+        //-- 100 centésimas hacen 1 segundo
+        if (this.cent == 100) {
         this.seg += 1;
         this.cent = 0;
-    }
-    //60 segundos es un minuto 
-    if (this.seg == 60) {
+        }
+
+        //-- 60 segundos hacen un minuto
+        if (this.seg == 60) {
         this.min = 1;
         this.seg = 0;
+        }
+
+        //-- Mostrar el valor actual
+        this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
     }
-    //Mostrar el valor actual
-    this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
-}
 
-//Arrancar el cronometro
-start() {
-    if (!this.timer) {
-        //Lanzar el temporizador para que llame
-        //al metodo tic cada 10ms(una centesima)
-        this.timer = setInterval(() => {
-            this.tic();
-        }, 10);
+    //-- Arrancar el cronómetro
+    start() {
+       if (!this.timer) {
+          //-- Lanzar el temporizador para que llame 
+          //-- al método tic cada 10ms (una centésima)
+          this.timer = setInterval( () => {
+              this.tic();
+          }, 10);
+        }
     }
-}
 
-//Parar el cronometro
-stop() {
-    if(this.timer) {
-        clearInterval(this.timer);
-        this.timer = null;
+    //-- Parar el cronómetro
+    stop() {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
     }
-}
 
-//Reset del cronometro
-reset() {
-    this.cent = 0;
-    this.seg = 0;
-    this.min = 0;
+    //-- Reset del cronómetro
+    reset() {
+        this.cent = 0;
+        this.seg = 0;
+        this.min = 0;
 
-    this.display.innerHTML = "0:0:0";
+        this.display.innerHTML = "0:0:0";
     }
 }
