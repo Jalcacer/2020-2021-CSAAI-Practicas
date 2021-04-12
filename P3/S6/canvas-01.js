e93c85df342afba249efef6f769ca2ed5647d04d
alcacer <j.alcacer.2018@alumnos.urjc.es>
2021-04-12 16:51:19 +0200

    Ejemplo 2

--- P3/S6/canvas-01.js
+++ P3/S6/canvas-01.js
@@ -4,3 +4,24 @@ const canvas = document.getElementById("canvas");
 // Definir el tamaño del canvas
 canvas.width = 300;
 canvas.height = 100;
+
+//EJEMPLO 2
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Cada objeto a dibujar lo delimitaremos 
+//-- por los métodos beginPath() y closePath()
+ctx.beginPath();
+  //-- Definir un rectangulo de dimensiones 100x50,
+  //-- cuya esquina superior izquierda está en (5,5)
+  ctx.rect(5,5, 100, 50);
+
+  //-- Color de relleno del rectángulo
+  ctx.fillStyle = 'blue';
+
+  //-- Mostrar el relleno
+  ctx.fill();
+
+  //-- Mostrar el trazo del rectángulo
+  ctx.stroke();
+ctx.closePath();
