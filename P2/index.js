b3dfd3b7c226762df1a2908d5a75a927ab408b54
alcacer <j.alcacer.2018@alumnos.urjc.es>
2021-05-01 12:46:47 +0200

    llamo a las variables desde javascript

---
+++ P2/index.js
@@ -0,0 +1,5 @@
+//Lo primero es llamar a las variables del codigo html para trabajar con ellas desde aqui
+display = document.getElementById("display");
+igual = document.getElementById("igual");
+borrar = document.getElementById(borrar);
+reset = document.getElementById(borrar);
