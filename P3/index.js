ddb8ac8d6a4402e30cf15ee87307f98445739e31
alcacer <j.alcacer.2018@alumnos.urjc.es>
2021-05-04 11:29:52 +0200

    dibujo la pelota

--- P3/index.js
+++ P3/index.js
@@ -2,6 +2,24 @@ console.log("Ejecutando JS...");
 
 const canvas = document.getElementById("canvas");
 
-//-- Definir el tamaño del convas
+//-- Definir el tamaño del canvas
 canvas.width = 520;
 canvas.height = 400;
+
+const ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+    //-- Dibujar un circulo: coordenadas x,y del centro
+    //-- Radio, Angulo inicial y angulo final
+    ctx.arc(100, 50, 10, 0, 2 * Math.PI);
+    ctx.lineWidth = 3;
+    ctx.fillStyle = 'red';
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+
+    //-- Dibujar el relleno
+    ctx.fill()
+    
+ctx.closePath()
