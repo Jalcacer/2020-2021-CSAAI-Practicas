3421746dc754d8397bdc3ee4074fbab9ff00c5e5
alcacer <j.alcacer.2018@alumnos.urjc.es>
2021-05-17 11:14:16 +0200

    Llamo a las id del html

--- P4/index.js
+++ P4/index.js
@@ -4,8 +4,12 @@ console.log("Ejecutando JS....")
 const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagen1');
 const ctx = canvas.getContext('2d');
-
-
+const R = document.getElementById("R");
+const G = document.getElementById("G");
+const B = document.getElementById("B");
+const range_value_R = document.getElementById("range_value_R");
+const range_value_G = document.getElementById("range_value_G");
+const range_value_B = document.getElementById("range_value_B");
 imagen1.onload = function () {
 
   console.log("Imagen cargada");
@@ -16,6 +20,7 @@ imagen1.onload = function () {
 //-- No se han hecho manipulaciones todavia
 ctx.drawImage(img, 0,0);
 }
+
 //Funcion deslizadores
 function RGB (){
   range_value_R.innerHTML = R.value;
@@ -32,9 +37,9 @@ function RGB (){
   let data = imgData.data;
 
 //Obtenemos los umbrales de los deslizadores
-  umbral_R = R.value
-  umbral_G = G.value
-  umbral_B = B.value
+  umbral_R = R.value;
+  umbral_G = G.value;
+  umbral_B = B.value;
 //Cambiamos la imagen segun el nuevo umbral
 
   for (let i = 0; i < data.length; i+=4) {
